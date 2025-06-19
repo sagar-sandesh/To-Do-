0c7a47dca12c0e286d5ea86afab48f081ae14e3b
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-06-20 00:45:23 +0300

    initial commit

---
+++ static/js/voice.js
@@ -0,0 +1,10 @@
+function startVoiceInput() {
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition.lang = 'en-US';
+    recognition.start();
+
+    recognition.onresult = function(event) {
+        const transcript = event.results[0][0].transcript;
+        document.getElementById('taskInput').value = transcript;
+    };
+}
