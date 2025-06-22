26cbe9f13259c1c1bc7b02ad6ceb495343b696e7
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-06-22 21:36:03 +0300

    Adding the username segment in register form

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def load_user(user_id):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
+    username=StringField('username', validators=[DataRequired(),Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
