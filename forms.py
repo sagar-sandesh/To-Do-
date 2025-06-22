d8b7aecdf7e188af39ca812a6cb68a6189c1aba2
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-06-22 21:34:38 +0300

    removing of unused part

--- forms.py
+++ forms.py
@@ -20,17 +20,3 @@ class TaskForm(FlaskForm):
     priority = StringField('Priority (e.g., High, Medium, Low)')
     category = StringField('Category (e.g., Work, Home)')
     submit = SubmitField('Add/Update Task')
-
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField, SubmitField
-# from wtforms.validators import DataRequired, Length
-#
-# class RegisterForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Register')
-#
-# class LoginForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Login')
